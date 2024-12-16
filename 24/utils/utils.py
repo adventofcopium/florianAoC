61261c7742c667d4190b06e0a7225e5ea503346e
ge65kem <florian.hunecke@tum.de>
2024-12-16 07:15:58 +0100

    update nb

--- 24/utils/utils.py
+++ 24/utils/utils.py
@@ -15,13 +15,25 @@ class Vec:
         return self if k == 0 else Vec(-self.y, self.x) >> k-1
     def __lshift__(self, k):
         return self if k == 0 else Vec(self.y, -self.x) << k-1
+    def __mul__(self, other):
+        return Vec(self.x * other, self.y * other)
+    def __mod__(self, other):
+        if isinstance(other, int):
+            return Vec(self.x % other, self.y % other)
+        elif isinstance(other, Vec):
+            return Vec(self.x % other.x, self.y % other.y)
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+    def __le__(self, other):
+        return self.x <= other.x and self.y <= other.y
     def __hash__(self):
         return hash(repr(self))
     def get_neighbors(self, bound:"Matrix"=None):
         neighbors = [self + dir for dir in [Vec(0,1), Vec(-1,0), Vec(0,-1), Vec(1,0)]]
         return [nb for nb in neighbors if (nb in bound)] if bound is not None else neighbors
+    def update(self, dir):
+        self.x += dir.x
+        self.y += dir.y
     
 class Matrix(np.ndarray):
     def __new__(cls, input_array):
@@ -39,6 +51,10 @@ class Matrix(np.ndarray):
             m, n = self.shape
             return 0 <= pos.x < m and 0 <= pos.y < n
         return super().__contains__(pos)
+    def __repr__(self):
+        return np.ndarray.__repr__(self.T)
+    def __str__(self):
+        return "\n".join(["".join(line) for line in self.T.tolist()])
     @staticmethod
     def from_str(s:str):
-        return Matrix(np.array(list(map(list, s.read().split("\n")))).T)
+        return Matrix(np.array(list(map(list, s.split("\n")))).T)
